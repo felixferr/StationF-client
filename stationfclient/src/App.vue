1bacb65158e0ff67a6790874d8cdce897e8e4766
felixferr <felixok@live.fr>
2019-09-03 23:54:06 +0200

    installation de vuetify et mise en place du premier template

---
+++ stationfclient/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
