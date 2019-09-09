4564c91979187c6786b54e0fa131a852cd777235
felixferr <felixok@live.fr>
2019-09-09 23:51:19 +0200

    maj equipements if empty

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,13 +21,15 @@
                 label="Nombre de personne"
               ></v-select>
 
-              <v-select class="pa-6 "
-              v-model="search" 
-              solo-inverted 
-              :items="rooms" 
-              multiple 
-              persistent-hint
-              label="Votre équipement"
+              <v-select
+                class="pa-6"
+                v-model="search"
+                solo-inverted
+                :items="rooms"
+                item-text="equipements[0].name"
+                multiple
+                persistent-hint
+                label="Votre équipement"
               ></v-select>
 
               <div class="flex-grow-1"></div>
@@ -35,25 +37,32 @@
           </v-row>
         </template>
 
-        <template v-slot:default="props" >
-          <v-row >
-            <v-col v-for="room in props.items" :key="room.name" cols="12" sm="6" md="4" lg="3">
-              <v-card height="100%" color="transparent"  v-if="show" >
-                <v-card-title class="subheading font-weight-bold" >{{ room.name }}</v-card-title>
+        <template v-slot:default="props">
+          <v-row>
+            <v-col v-for="(room, index) in props.items" :key="room.id" cols="12" sm="6" md="4" lg="3">
+              <v-card height="100%" color="transparent" v-if="show">
+                <v-card-title class="subheading font-weight-bold">{{ room.name }}</v-card-title>
 
                 <v-divider></v-divider>
 
                 <v-list color="transparent" class="ma-3">
                   <v-list-item-content class="align-end">Déscription : {{ room.name }}</v-list-item-content>
                   <v-list-item-content class="align-end">Capacité : {{ room.capacity }}</v-list-item-content>
-                  <v-list-item-content class="align-end">Equipements : {{ room.equipements }}</v-list-item-content>
+                  <v-list-item-content
+                    class="align-end"
+                  >Equipements : {{room.equipements && room.equipements.length > 0 ? room.equipements[0].name : ''}}, {{room.equipements && room.equipements.length > 1 ? room.equipements[1].name : ''}}</v-list-item-content>
                   <v-list-item-content class="align-end">Horaires :</v-list-item-content>
-                  <v-btn class="ma-2" outlined color="indigo">9:00</v-btn>
+                  <v-btn  class="ma-2" outlined color="indigo">9:00</v-btn>
                   <v-btn class="ma-2" outlined color="indigo">10:30</v-btn>
                   <v-btn class="ma-2" outlined color="indigo">14:30</v-btn>
                   <v-btn class="ma-2" outlined color="indigo">16:30</v-btn>
                   <div class="text-center">
-                    <v-btn v-on:click="getById(room._id)" @click="snackbar = true" outlined color="#00E676">Réserver</v-btn>
+                    <v-btn
+                      v-on:click="getById(room._id)"
+                      @click="snackbar = true"
+                      outlined
+                      color="#00E676"
+                    >Réserver</v-btn>
                     <div class="text-center">
                       <v-snackbar color="green" v-model="snackbar" :timeout="timeout">{{ text }}</v-snackbar>
                     </div>
@@ -79,7 +88,7 @@ export default {
       rooms: [],
       snackbar: false,
       text: "Réservation confirmée !",
-      timeout: 2000
+      timeout: 2000,
     };
   },
 
@@ -92,24 +101,23 @@ export default {
 
   methods: {
     getById(id) {
-      axios.get(`http://localhost:3000/api/v1/rooms/${id}`).then(response => {
-        
-      });
+      axios
+        .get(`http://localhost:3000/api/v1/rooms/${id}`)
+        .then(response => {});
     },
-    setSelected(value){
-      this.show=true
-      
+    setSelected(value) {
+      this.show = true;
     }
   }
 };
 </script>
 <style scoped>
- @import url('http://fonts.googleapis.com/css?family=Rubik&display=swap');
+@import url("http://fonts.googleapis.com/css?family=Rubik&display=swap");
 
-    #inspire {
-     font-family: 'Rubik', sans-serif;
-     font-size:  17px;
-    }
+#inspire {
+  font-family: "Rubik", sans-serif;
+  font-size: 17px;
+}
 
 img {
   display: block;
@@ -117,6 +125,4 @@ img {
   margin-right: auto;
   width: 20%;
 }
-
-
 </style>
